4cc294e75baa60abf4146be7a061c68ef89f2237
renatabagdanova <bagdanova.renata@gmail.com>
2025-10-28 16:50:06 +0300

    commit 3

--- app/src/main/java/ru/practicum/android/diploma/util/debounce/DebounceConfig.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/debounce/DebounceConfig.kt
@@ -2,6 +2,6 @@ package ru.practicum.android.diploma.util.debounce
 
 object DebounceConfig {
     const val SEARCH_DELAY = 300L // 300ms для поиска
-    const val CLICK_DELAY = 500L  // 500ms для кликов
+    const val CLICK_DELAY = 500L // 500ms для кликов
     const val TYPING_DELAY = 150L // 150ms для ввода текста
 }
