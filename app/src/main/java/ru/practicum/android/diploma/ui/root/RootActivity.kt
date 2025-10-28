67372c37f0bbe2f1b64823d5aa56cb57e4601bec
Maksim <lamand704@gmail.com>
2025-10-28 13:53:36 +0300

    нижняя панель навигации

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,20 +1,60 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.root)
 
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = "")
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.let {
+            NavigationUI.setupWithNavController(it, navController)
+            it.itemBackground = ContextCompat.getDrawable(this, android.R.color.transparent)
+        }
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.filterSettingsFragment, R.id.chooseRegionFragment, R.id.chooseCountryFragment, R.id.chooseIndustryFragment, R.id.chooseWorkPlaceFragment, R.id.vacancyFragment2 -> {
+                    hideBottomNavigationView()
+                }
+                else -> {
+                    showBottomNavigationView()
+                }
+            }
+        }
+    }
+    private fun hideBottomNavigationView() {
+        bottomNavigationView.visibility = View.GONE
     }
 
-    private fun networkRequestExample(accessToken: String) {
-        // ...
+    private fun showBottomNavigationView() {
+        bottomNavigationView.visibility = View.VISIBLE
     }
-
 }
