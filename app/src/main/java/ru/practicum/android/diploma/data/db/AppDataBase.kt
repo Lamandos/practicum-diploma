b8abf3b3c1e058e891bb11cee3cb5db565ff45db
renatabagdanova <bagdanova.renata@gmail.com>
2025-10-31 18:29:38 +0300

    commit 2

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDataBase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDataBase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database (entities = [FavoritesEntity::class], version = 3)
-abstract class AppDataBase: RoomDatabase() {
+@Database(entities = [FavoritesEntity::class], version = 3)
+abstract class AppDataBase : RoomDatabase() {
     abstract fun favoritesDao(): FavoritesDao;
+
     companion object{
         fun getDb(context: Context): AppDataBase{
             return Room.databaseBuilder(
