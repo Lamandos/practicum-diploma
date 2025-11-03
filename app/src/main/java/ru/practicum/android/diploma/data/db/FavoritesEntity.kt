37cd8a2a4017f67ce8457794e9e2bee2cf3831fb
renatabagdanova <bagdanova.renata@gmail.com>
2025-11-03 11:44:44 +0300

    commit 1

--- app/src/main/java/ru/practicum/android/diploma/data/db/FavoritesEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/FavoritesEntity.kt
@@ -14,42 +14,26 @@ data class FavoritesEntity(
     val name: String,
     @ColumnInfo(name = "description")
     val description: String,
-    @ColumnInfo(name = "salary_from")
-    val salaryFrom: Int?,
-    @ColumnInfo(name = "salary_to")
-    val salaryTo: Int?,
-    @ColumnInfo(name = "salary_currency")
-    val salaryCurrency: String?,
+    @ColumnInfo(name = "salary")
+    val salary: String?,
     @ColumnInfo(name = "address")
     val address: String?,
-    @ColumnInfo(name = "experience_id")
-    val experienceId: String,
-    @ColumnInfo(name = "experience_name")
-    val experienceName: String,
-    @ColumnInfo(name = "schedule_id")
-    val scheduleId: String,
-    @ColumnInfo(name = "schedule_name")
-    val scheduleName: String,
-    @ColumnInfo(name = "employment_id")
-    val employmentId: String,
-    @ColumnInfo(name = "employment_name")
-    val employmentName: String,
-    @ColumnInfo(name = "employer_id")
-    val employerId: String,
-    @ColumnInfo(name = "employer_name")
-    val employerName: String,
-    @ColumnInfo(name = "employer_logo")
-    val employerLogo: String?,
-    @ColumnInfo(name = "area_id")
-    val areaId: String,
-    @ColumnInfo(name = "area_name")
-    val areaName: String,
+    @ColumnInfo(name = "experience")
+    val experience: String,
+    @ColumnInfo(name = "schedule")
+    val schedule: String,
+    @ColumnInfo(name = "employment")
+    val employment: String,
+    @ColumnInfo(name = "employer")
+    val employer: String,
+    @ColumnInfo(name = "area")
+    val area: String,
     @ColumnInfo(name = "skills")
     val skills: List<String>,
     @ColumnInfo(name = "url")
     val url: String,
-    @ColumnInfo(name = "industry_id")
-    val industryId: String,
-    @ColumnInfo(name = "industry_name")
-    val industryName: String,
+    @ColumnInfo(name = "industry")
+    val industry: String,
+    @ColumnInfo(name = "published")
+    val published: String?
 )
---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/Mappers.kt
@@ -0,0 +1,42 @@
+package ru.practicum.android.diploma.data.db
+
+import ru.practicum.android.diploma.domain.models.vacancydetails.Address
+import ru.practicum.android.diploma.domain.models.vacancydetails.Salary
+import ru.practicum.android.diploma.domain.models.vacancydetails.VacancyDetails
+
+class Mappers {
+    fun FavoritesEntity.toModel(): VacancyDetails = VacancyDetails(
+        id = id,
+        name = name,
+        description = description,
+        salary = salary as Salary?,
+        address = address as Address?,
+        experience = experience,
+        schedule = schedule,
+        employer = employer,
+        contacts = null,
+        area = area,
+        skills = skills,
+        url = url,
+        industry = industry,
+        publishedAt = published,
+        employment = employment
+    )
+
+    fun VacancyDetails.toEntity(): FavoritesEntity = FavoritesEntity(
+        id = id,
+        name = name,
+        description = description,
+        salary = salary as String?,
+        address = address as String?,
+        experience = experience,
+        schedule = schedule,
+        employment = employment,
+        employer = employer,
+        area = area,
+        skills = skills,
+        url = url,
+        industry = industry,
+        published = publishedAt
+    )
+}
