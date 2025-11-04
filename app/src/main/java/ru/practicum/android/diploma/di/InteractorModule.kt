6ae1c262f085fc1e6000859e6f85e8e7adc8b7cd
Vadim <vadshustow@ya.ru>
2025-11-05 02:47:56 +0700

    DI для поиска вакансий

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,6 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.api.usecases.VacanciesInteractor
+import ru.practicum.android.diploma.domain.impl.usecases.VacanciesInteractorImpl
 
 val interactorModule = module {
+
+    factory<VacanciesInteractor> {
+        VacanciesInteractorImpl(get())
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,6 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.repository.VacanciesRepositoryImpl
+import ru.practicum.android.diploma.domain.api.repositories.VacanciesRepository
 
 val repositoryModule = module {
+
+    single<VacanciesRepository> {
+        VacanciesRepositoryImpl(get())
+    }
 }
