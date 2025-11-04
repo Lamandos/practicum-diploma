07d71a4a708b655ecd834ab57c81ec0cf247ec30
Vadim <vadshustow@ya.ru>
2025-11-04 19:27:49 +0700

    di для поиска вакансий

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,12 +1,12 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.search.viewmodel.SearchViewModel
 
 val viewModelModule = module {
+
     viewModel {
         SearchViewModel(get())
     }
 }
-
