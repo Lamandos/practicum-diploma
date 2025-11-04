e69e8dc5a55b7bb47b99871655c1d24616fff916
Vadim <vadshustow@ya.ru>
2025-11-04 23:02:03 +0700

    убрал неиспользуемые импорты

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/VacancyFragment.kt
@@ -2,9 +2,7 @@ package ru.practicum.android.diploma.ui.screens
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.util.Linkify
 import android.view.View
-import androidx.core.text.util.LinkifyCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
