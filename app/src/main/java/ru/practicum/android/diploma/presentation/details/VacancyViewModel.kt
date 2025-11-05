f8d672aed224ca79d0a0a5ffda9841f09ab46ad9
renatabagdanova <bagdanova.renata@gmail.com>
2025-11-05 18:09:27 +0300

    commit 11

--- app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyViewModel.kt
@@ -109,8 +109,14 @@ class VacancyViewModel(
         try {
             favoritesInteractor.removeFromFavorites(vacancyId)
             _isFavorite.value = false
-        } catch (e: Exception) {
-            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites", e)
+        } catch (e: IOException) {
+            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - IOException", e)
+            throw e
+        } catch (e: SecurityException) {
+            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - SecurityException", e)
+            throw e
+        } catch (e: IllegalStateException) {
+            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - IllegalStateException", e)
             throw e
         }
     }
@@ -126,8 +132,14 @@ class VacancyViewModel(
             } else {
                 handleError(ERROR_LOAD_VACANCY)
             }
-        } catch (e: Exception) {
-            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites", e)
+        } catch (e: IOException) {
+            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - IOException", e)
+            throw e
+        } catch (e: SecurityException) {
+            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - SecurityException", e)
+            throw e
+        } catch (e: IllegalStateException) {
+            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - IllegalStateException", e)
             throw e
         }
     }
