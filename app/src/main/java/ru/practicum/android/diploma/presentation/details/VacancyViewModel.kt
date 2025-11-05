ccbb92889401dafa3fdd01b5a673cd483457392b
renatabagdanova <bagdanova.renata@gmail.com>
2025-11-05 18:12:20 +0300

    commit 12

--- app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyViewModel.kt
@@ -109,14 +109,12 @@ class VacancyViewModel(
         try {
             favoritesInteractor.removeFromFavorites(vacancyId)
             _isFavorite.value = false
-        } catch (e: IOException) {
-            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - IOException", e)
-            throw e
-        } catch (e: SecurityException) {
-            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - SecurityException", e)
-            throw e
-        } catch (e: IllegalStateException) {
-            handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - IllegalStateException", e)
+        } catch (e: Exception) {
+            when (e) {
+                is IOException, is SecurityException, is IllegalStateException -> {
+                    handleErrorWithLog(ERROR_REMOVE_FAVORITE, "removeFromFavorites - ${e::class.simpleName}", e)
+                }
+            }
             throw e
         }
     }
@@ -132,14 +130,12 @@ class VacancyViewModel(
             } else {
                 handleError(ERROR_LOAD_VACANCY)
             }
-        } catch (e: IOException) {
-            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - IOException", e)
-            throw e
-        } catch (e: SecurityException) {
-            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - SecurityException", e)
-            throw e
-        } catch (e: IllegalStateException) {
-            handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - IllegalStateException", e)
+        } catch (e: Exception) {
+            when (e) {
+                is IOException, is SecurityException, is IllegalStateException -> {
+                    handleErrorWithLog(ERROR_ADD_FAVORITE, "addToFavorites - ${e::class.simpleName}", e)
+                }
+            }
             throw e
         }
     }
